c9c718ef06f09b69db609606272b468c3a5ecbea
sidharth-vk <sidharthd717@gmail.com>
2023-09-21 00:33:26 +0530

    version 1 commit

--- webflow/groups/urls.py
+++ webflow/groups/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
 
     
 path('',groups,name="groups"),
-path('<pk>',groups_view,name="groups_view")
+path('<pk>',groups_view,name="groups_view"),
+path('grp_add/<pk>',join_grp,name="join_grp")
 
 ] 
--- webflow/groups/views.py
+++ webflow/groups/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import *
 from authentication.models import *
+from django.shortcuts import HttpResponseRedirect, get_object_or_404
+from django.db.models import Q
+
 # Create your views here.
 
 
@@ -22,6 +25,10 @@ def groups_view(request,pk):
     grp_feeds = grp_feed.objects.filter(group=groups_instance)
     grp_skill = grp_skills.objects.filter(group=groups_instance)
     grp_talks_abouts = grp_talks_about.objects.filter(group=groups_instance)
+    current_user_profile = profiles.objects.get(user=request.user)
+
+
+    grp_member_grp = grp_member.objects.filter(Q(user=current_user_profile) & Q(group= groups_instance))
 
     context = {
         'all_groups':all_groups,
@@ -29,6 +36,15 @@ def groups_view(request,pk):
         'grp_feeds':grp_feeds,
         'group_member_total':group_member_total,
         "grp_skill":grp_skill,
-        'grp_talks_abouts':grp_talks_abouts
+        'grp_talks_abouts':grp_talks_abouts,
+        "grp_member_grp":grp_member_grp
     }
     return render(request,'groups/groups_views.html',context)
+
+
+def join_grp(request,pk):
+    groups_instance = group.objects.get(slug=pk)
+    current_user_profile = profiles.objects.get(user=request.user)
+    grp_member.objects.create(group=groups_instance,user=current_user_profile)
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
